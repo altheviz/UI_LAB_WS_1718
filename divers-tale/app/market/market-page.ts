c516cd2c10de167ee581250b8e848d8a33e69a91
Heurazio <alexander.hasel@hasel.de>
2017-12-11 20:52:47 +0100

    tmp

---
+++ divers-tale/app/market/market-page.ts
@@ -0,0 +1,38 @@
+import { EventData } from "data/observable";
+import { RadSideDrawer } from "nativescript-pro-ui/sidedrawer";
+import { topmost } from "ui/frame";
+import { NavigatedData, Page } from "ui/page";
+
+import {MarketService} from "./market-service";
+import {MarketViewModel} from "./market-view-model";
+
+const ms = new MarketViewModel();
+
+export function onNavigatingTo(args: NavigatedData) {
+    if (args.isBackNavigation) {
+        return;
+    }
+    const pageName: string = args.context.pageName;
+    
+    const page = <Page>args.object;
+
+    page.bindingContext = ms;
+    // TODO ...
+}
+
+export function openListItem(args: EventData){
+    var navigationEntry = {
+        moduleName: "details/details-page",
+        context: {
+            fItem: ms.get("list")[args["index"]],
+            parentPageName: ms.get("selectedPage")
+        },
+        animated: false
+    };
+    topmost().navigate(navigationEntry); 
+}
+
+export function onDrawerButtonTap(args: EventData) {
+    const sideDrawer = <RadSideDrawer>topmost().getViewById("sideDrawer");
+    sideDrawer.showDrawer();
+}
