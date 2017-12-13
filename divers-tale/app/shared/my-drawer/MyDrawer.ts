610e3ab0103a8c8dd5db453142e230e906ad7214
Frederik Martin <frederik.martin@gmail.com>
2017-12-13 06:27:20 +0100

    Clear history stack on logout

--- divers-tale/app/shared/my-drawer/MyDrawer.ts
+++ divers-tale/app/shared/my-drawer/MyDrawer.ts
@@ -5,9 +5,6 @@ import { GridLayout } from "ui/layouts/grid-layout";
 import { MyDrawerViewModel } from "./MyDrawer-view-model";
 import { AuthService } from "../../authentication/authentication-service";
 
-/* ***********************************************************
-* Use the "loaded" event handler of the wrapping layout element to bind the view model to your view.
-*************************************************************/
 export function onLoaded(args: EventData): void {
     const component = <GridLayout>args.object;
     const componentTitle = component.get("selectedPage");
@@ -15,11 +12,6 @@ export function onLoaded(args: EventData): void {
     component.bindingContext = new MyDrawerViewModel(componentTitle);
 }
 
-/* ***********************************************************
-* Use the "tap" event handler of the <GridLayout> component for handling navigation item taps.
-* The "tap" event handler of the app drawer <GridLayout> item is used to navigate the app
-* based on the tapped navigationItem's route.
-*************************************************************/
 export function onNavigationItemTap(args: EventData): void {
     const component = <GridLayout>args.object;
     const componentRoute = component.get("route");
@@ -39,10 +31,11 @@ export function onLogout(args: EventData): void {
             console.info("LOGOUT SUCCESSFUL");
             topmost().navigate({
                 moduleName: "authentication/login/login-page",
-                backstackVisible: false,
                 transition: {
                     name: "fade"
-                }
+                },
+                backstackVisible: false,
+                clearHistory: true
             });
         })
         .catch((err) => {
