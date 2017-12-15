cb59e0daa4a3ae1944c5876402ff8810150b6619
Frederik Martin <frederik.martin@gmail.com>
2017-12-15 22:43:04 +0100

    Finish settings module

--- divers-tale/app/settings/settings-page.ts
+++ divers-tale/app/settings/settings-page.ts
@@ -1,107 +1,140 @@
-import { EventData } from "data/observable";
+import { EventData, Observable } from "data/observable";
+import { ObservableArray } from "data/observable-array";
 import { RadSideDrawer } from "nativescript-pro-ui/sidedrawer";
 import { topmost } from "ui/frame";
 import { NavigatedData, Page } from "ui/page";
+import { ValueList, SelectedIndexChangedEventData, DropDown } from "nativescript-drop-down";
 
-import { SettingsViewModel } from "./settings-view-model";
+import { SettingsViewModel } from "./service-view-model";
+import { SettingService } from "./settings-service";
 
-//begin dropdown
-import observable = require("data/observable");
-import observableArray = require("data/observable-array");
-import pages = require("ui/page");
-import { SelectedIndexChangedEventData } from "nativescript-drop-down";
-let viewModel: observable.Observable;
-//end dropdown
+let viewModel: Observable;
 
-let email;
-let testPage;
-
-/* ***********************************************************
-* Use the "onNavigatingTo" handler to initialize the page binding context.
-*************************************************************/
 export function onNavigatingTo(args: NavigatedData) {
-    /* ***********************************************************
-    * The "onNavigatingTo" event handler lets you detect if the user navigated with a back button.
-    * Skipping the re-initialization on back navigation means the user will see the
-    * page in the same data state that he left it in before navigating.
-    *************************************************************/
     if (args.isBackNavigation) {
         return;
     }
 
-    console.log("onNavigatingTo");
     const page = <Page>args.object;
-    page.bindingContext = new SettingsViewModel();
+    viewModel = new SettingsViewModel();
+    page.bindingContext = viewModel;
 
-    testPage = page;
     initDropdown(page);
 }
 
-/* ***********************************************************
-* According to guidelines, if you have a drawer on your page, you should always
-* have a button that opens it. Get a reference to the RadSideDrawer view and
-* use the showDrawer() function to open the app drawer section.
-*************************************************************/
+function initDropdown(page: Page) {
+    // Measurement unit
+    let measurementUnitList = page.getViewById<DropDown>("measurement-unit");
+    let measurementUnitItems = new ValueList<String>([
+        { value: SettingService.METER, display: "Meter" },
+        { value: SettingService.FEET, display: "Feet" }
+    ]);
+    if (viewModel.get("settings").getMeasurementUnit() === SettingService.METER) {
+        viewModel.set("measurementUnitSelectedIndex", 0);
+    } else {
+        viewModel.set("measurementUnitSelectedIndex", 1);
+    }
+    measurementUnitList.items = measurementUnitItems;
+
+    // Pressure unit
+    let pressureUnitList = page.getViewById<DropDown>("pressure-unit");
+    let pressureUnitItems = new ValueList<String>([
+        { value: SettingService.BAR, display: "Bar" },
+        { value: SettingService.PSI, display: "PSI" }
+    ]);
+    if (viewModel.get("settings").getPressureUnit() === SettingService.BAR) {
+        viewModel.set("pressureUnitSelectedIndex", 0);
+    } else {
+        viewModel.set("pressureUnitSelectedIndex", 1);
+    }
+    pressureUnitList.items = pressureUnitItems;
+
+    // Temperature unit
+    let temperatureUnitList = page.getViewById<DropDown>("temperature-unit");
+    let temperatureUnitItems = new ValueList<String>([
+        { value: SettingService.CELSIUS, display: "Celsius" },
+        { value: SettingService.FAHRENHEIT, display: "Fahrenheit" }
+    ]);
+    if (viewModel.get("settings").getTemperatureUnit() === SettingService.CELSIUS) {
+        viewModel.set("temperatureUnitSelectedIndex", 0);
+    } else {
+        viewModel.set("temperatureUnitSelectedIndex", 1);
+    }
+    temperatureUnitList.items = temperatureUnitItems;
+
+    // Time format
+    let timeFormatList = page.getViewById<DropDown>("time-format");
+    let timeFormatItems = new ValueList<String>([
+        { value: SettingService.TWENTYFOUR_HOURS, display: "24 Hours" },
+        { value: SettingService.AM_PM, display: "AM/PM" }
+    ]);
+    if (viewModel.get("settings").getTimeFormat() === SettingService.TWENTYFOUR_HOURS) {
+        viewModel.set("timeFormatSelectedIndex", 0);
+    } else {
+        viewModel.set("timeFormatSelectedIndex", 1);
+    }
+    timeFormatList.items = timeFormatItems;
+
+    // Date format
+    let dateFormatList = page.getViewById<DropDown>("date-format");
+    let dateFormatItems = new ValueList<String>([
+        { value: SettingService.DD_MM_YYYY, display: "dd.mm.yyyy" },
+        { value: SettingService.MM_DD_YYYY, display: "mm/dd/yyyy" }
+    ]);
+    if (viewModel.get("settings").getDateFormat() === SettingService.DD_MM_YYYY) {
+        viewModel.set("dateFormatSelectedIndex", 0);
+    } else {
+        viewModel.set("dateFormatSelectedIndex", 1);
+    }
+    dateFormatList.items = dateFormatItems;
+}
+
+export function measurementUnitChanged(args: SelectedIndexChangedEventData) {
+    if (args.newIndex === 0 || args.newIndex === null) {
+        viewModel.get("settings").setMeasurementUnit(SettingService.METER);
+    } else {
+        viewModel.get("settings").setMeasurementUnit(SettingService.FEET);
+    }
+    SettingService.saveSettings();
+}
+
+export function pressureUnitChanged(args: SelectedIndexChangedEventData) {
+    if (args.newIndex === 0 || args.newIndex === null) {
+        viewModel.get("settings").setPressureUnit(SettingService.BAR);
+    } else {
+        viewModel.get("settings").setPressureUnit(SettingService.PSI);
+    }
+    SettingService.saveSettings();
+}
+
+export function temperatureUnitChanged(args: SelectedIndexChangedEventData) {
+    if (args.newIndex === 0 || args.newIndex === null) {
+        viewModel.get("settings").setTemperatureUnit(SettingService.CELSIUS);
+    } else {
+        viewModel.get("settings").setTemperatureUnit(SettingService.FAHRENHEIT);
+    }
+    SettingService.saveSettings();
+}
+
+export function timeFormatChanged(args: SelectedIndexChangedEventData) {
+    if (args.newIndex === 0 || args.newIndex === null) {
+        viewModel.get("settings").setTimeFormat(SettingService.TWENTYFOUR_HOURS);
+    } else {
+        viewModel.get("settings").setTimeFormat(SettingService.AM_PM);
+    }
+    SettingService.saveSettings();
+}
+
+export function dateFormatChanged(args: SelectedIndexChangedEventData) {
+    if (args.newIndex === 0 || args.newIndex === null) {
+        viewModel.get("settings").setDateFormat(SettingService.DD_MM_YYYY);
+    } else {
+        viewModel.get("settings").setDateFormat(SettingService.MM_DD_YYYY);
+    }
+    SettingService.saveSettings();
+}
+
 export function onDrawerButtonTap(args: EventData) {
-    console.log("onDrawerButtonTap");
     const sideDrawer = <RadSideDrawer>topmost().getViewById("sideDrawer");
     sideDrawer.showDrawer();
-}
-
-// for dropdown
-function initDropdown(page: pages.Page) {
-    console.log("pageLoaded");
-    //var page = <pages.Page>args.object;
-    var items = new observableArray.ObservableArray();
-    items.push("meter");
-    items.push("feet");
-
-    var press = new observableArray.ObservableArray();
-    press.push("bar");
-    press.push("psi");
-
-    var temperature = new observableArray.ObservableArray();
-    temperature.push("celsius");
-    temperature.push("fahrenheit");
-
-    var timeFormat = new observableArray.ObservableArray();
-    timeFormat.push("24 hours");
-    timeFormat.push("am/pm");
-
-    var dateFormat = new observableArray.ObservableArray();
-    dateFormat.push("dd.mm.yyyy");
-    dateFormat.push("mm/dd/yyyy");
-
-
-    viewModel = new observable.Observable();
-    viewModel.set("items", items);
-    viewModel.set("press", press);
-    viewModel.set("temperature",temperature);
-    viewModel.set("timeFormat", timeFormat);
-    viewModel.set("dateFormat", dateFormat);
-
-    viewModel.set("selectedIndex", 1);
-
-    page.bindingContext = viewModel;
-}
-
-export function dropDownOpened(args: EventData) {
-    console.log("dropDownOpened");
-    console.log("Drop Down opened");
-}
-
-export function dropDownClosed(args: EventData) {
-    console.log("dropDownClosed");
-    console.log("Drop Down closed");
-}
-
-export function dropDownSelectedIndexChanged(args: SelectedIndexChangedEventData) {
-    console.log("dropDownSelectedIndexChanged");
-    console.log(`Drop Down selected index changed from ${args.oldIndex} to ${args.newIndex}`);
-}
-
-export function showme() {
-    console.log("showme");
-    email = testPage.getViewById("email");
-    alert(email.text);
 }
